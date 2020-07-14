855f83afd74035d9ebf8c0d7d23fec3874552827
Jason Woolard <woolardjason@gmail.com>
2020-07-14 15:20:14 -0400

    module 4 complete, passed tests

--- src/components/BookList.vue
+++ src/components/BookList.vue
@@ -1,8 +1,9 @@
 <template>
   <div>
     <h1>{{title}}</h1>
+    <input type="text" placeholder="Search Books" v-model="searchInput" />
     <ul>
-      <book-item v-for="book in books" :key="book.id" :book="book"></book-item>
+      <book-item v-for="book in searchedBooks" :key="book.id" :book="book"></book-item>
     </ul>
     <hr />
     <h2>Filtered Books By Ownership</h2>
@@ -27,6 +28,7 @@ export default {
   name: "BookList",
   data() {
     return {
+      searchInput: "",
       holding: "bought",
       filters: ["bought", "borrowed"],
       title: "All Books",
@@ -60,6 +62,12 @@ export default {
   computed: {
     filteredBooks() {
       return _.filter(this.books, ["ownership", this.holding]);
+    },
+    searchedBooks() {
+      const searchFilter = book => {
+        return book.title.toLowerCase().match(this.searchInput.toLowerCase());
+      };
+      return _.filter(this.books, searchFilter);
     }
   },
   methods: {
